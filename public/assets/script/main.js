dc4764680437ff766e8d80b0f065036162526ba8
Zakk Fast <zakkfastpro@gmail.com>
2021-10-25 16:48:50 -0700

    init commit

---
+++ public/assets/script/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#bookNow').click(function(){
+        window.location = './contact.html'
+    })
+    $('#aboutMe').click(function(){
+        window.location = './about.html'
+    })
+})
