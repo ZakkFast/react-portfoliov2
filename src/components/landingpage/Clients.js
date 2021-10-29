9e4fd026e830fc161beb5dc2a72aec940bbf1fd0
Zakk Fast <zakkfastpro@gmail.com>
2021-10-28 23:09:26 -0700

    designed clients section for landing page

---
+++ src/components/landingpage/Clients.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+function Clients () {
+    return (
+        <div className="what-i-do-container">
+        <div className="what-i-do-title">
+            <h2>WHO I BUILD FOR</h2>
+        </div>
+        <div className="divider">
+            <div className="divider-div"></div>
+        </div>
+        <div className="service-container-main">
+            <div className="service-container">
+                <div className="service-title-container">
+                    <h3>Social Media Influencers</h3>
+                </div>
+                <div className="service-text-container">
+                    <p>As an Influencer your brand is everything, and I can help you build that brand.</p>
+                    <p>With a personal website you can direct your followers to a place an interactive place where you brand can truly shine. So let's give you and your fans the site you all deserve, and watch your brand grow to dizzying heights.</p>
+
+                </div>
+            </div>
+            <div className="service-container">
+                <div className="service-title-container">
+                    <h3>Photographers and Digital Artist</h3>
+                </div>
+                <div className="service-text-container">
+                    <p>Having a place to showcase your work can be the difference between rags and riches</p>
+                    <p>Anything from prints of you work, to merchandise featuring your art, I can build whatever it is you need to turn your art from into cash flow.</p>
+
+                </div>
+            </div>
+            <div className="service-container">
+                <div className="service-title-container">
+                    <h3>Content Creators</h3>
+                </div>
+                <div className="service-text-container">
+                    <p>Content creators are often at the mercy of whatever site your posting to..</p>
+                    <p>..and let's be honest, the terms are never in your favor. I can build you a site where you're no longer subjected to whatever some corporate suit says. It's you content, and your future. Take control of it.</p>
+                </div>
+            </div>
+            <div className="service-container">
+                <div className="service-title-container">
+                    <h3>Small Business</h3>
+                </div>
+                <div className="service-text-container">
+                    <p>Just because you're not some giant coporation doesn't mean you can't have a nice web site.</p>
+                    <p>Today more than every Small Business needs support. The giant companies make online shopping easy, and now you can too. With an e-commerce shop you can go from downtown shop, to global. Reach new clients across the world and grow your revenue like never before.</p>
+
+                </div>
+            </div>
+            
+            </div>
+        </div>
+    )
+}
+
+export default Clients;
