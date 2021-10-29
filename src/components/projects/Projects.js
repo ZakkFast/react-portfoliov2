40303cdf89f125f32019c69e716acafa108a3c03
Zakk Fast <zakkfastpro@gmail.com>
2021-10-28 23:05:41 -0700

    imported components

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,38 +1,22 @@
 import React, { Fragment } from 'react'
+import ProjectHead from './ProjectHead';
+import Project from './Project';
 
 function Projects () {
     return (
-        <div id="showcase">
-        <header>
-           <nav class="cf">
-               <ul class="cf">
-                   <li>
-                       <a href="index.html">Home</a>
-                   </li>
-                   <li>
-                       <a href="projects.html">What I Do</a>
-                   </li>
-                   <li>
-                      <a href="contact.html">Contact</a>
-                  </li>
-                </ul>
-               <a href="#" id="openup">Zakk Fast</a>
-           </nav> 
-        </header> 
-        <main class="container">
-            <h1>
-                PROJECTS
-            </h1>
-            <h2>
-                Here's some of the stuff I've worked on.
-            </h2>
-            <div class="divider">
-                <div class="divider-div"></div>
-            </div>
-        </main>
-    </div>
-    
+        <Fragment>
+        <ProjectHead />
+        <Project />
+        </Fragment>
     )
 }
 
 export default Projects;
+
+<section id="what-i-do">
+<div className="container">
+  <div className="what-i-do">
+    
+  </div>
+</div>
+</section>
