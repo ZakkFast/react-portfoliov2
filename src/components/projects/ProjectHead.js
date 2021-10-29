345f6fcc773bbcb0a894fd8aae1e5c4cd21978b8
Zakk Fast <zakkfastpro@gmail.com>
2021-10-28 23:06:09 -0700

    designed header for page

---
+++ src/components/projects/ProjectHead.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react'
+
+function ProjectHead () {
+    return (
+        <div className="section">
+        <div className="container mt-1">
+            <h1>
+                PROJECTS
+            </h1>
+            <h2>
+                Here's some of the stuff I've worked on.
+            </h2>
+            <div class="divider">
+                <div class="divider-div"></div>
+            </div>
+        </div>
+    </div>
+    
+    )
+}
+
+export default ProjectHead;
