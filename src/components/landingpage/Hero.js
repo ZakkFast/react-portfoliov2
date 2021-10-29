2cf5883571b38c3be9c3653e31955b1b816ab680
Zakk Fast <zakkfastpro@gmail.com>
2021-10-28 23:09:05 -0700

    set up hero for landing page

---
+++ src/components/landingpage/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Hero () {
+    return (
+        <div className="section">
+        <div className="container mt-1">
+            <h1 className="text-left margin-1 font-large">MY NAME IS ZAKK FAST..</h1>
+            <p className="lead text-left margin-1">..and I'm a Web Developer that stands out.</p>
+            <p className="lead text-left margin-1">There are plenty of developers content on making cookie-cutter websites. I didn't learn to code to make mediocre websites.</p>
+            <p className="lead text-left margin-1">I learned to code to make Web Apps that not only meet my client's needs, but inspire the people who use them. So if you're tired of looking at the same indefrentiable, uninspiring websites, then you're in the right place. </p>
+        </div>
+        {/* <div className="section-img-main"> */}
+        {/* <img src="assets/images/girl1.jpg" alt="Picture of a blonde girl"> */}
+    </div>
+    )
+}
+
+export default Hero;
